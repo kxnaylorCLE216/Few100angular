4abfce2a81bc7d30a78dedd306e27c54c59d7b4b
KYLE X NAYLOR <naylorkx@gmail.com>
2020-06-17 19:20:34 +0000

    stopping for the day

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TippingComponent } from './components/tipping/tipping.component';
+import { TodoComponent } from './components/todo/todo.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'tipping',
+    component: TippingComponent
+  },
+  {
+    path: 'todo',
+    component: TodoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TippingComponent } from './components/tipping/tipping.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TippingComponent
+    TippingComponent,
+    TodoComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
